8dfe4530d9af5b8b7ec96e47eae0890dc05341cc
Vladyslav Zaiets <rutova2@gmail.com>
2021-03-29 10:24:48 +0300

    cleanup in QueryBuilderTests

---
+++ tests/dotnet-micro-orm.Tests/ValidationHelperTests.cs
@@ -0,0 +1,48 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using DotnetMicroOrm.Utils;
+using FluentAssertions;
+
+namespace DotnetMicroOrm.Tests;
+
+public class ValidationHelperTests
+{
+    [Fact]
+    public void ValidatePassword_StrongPassword_ReturnsSuccess()
+    {
+        var (isValid, error) = ValidationHelper.ValidatePassword("SecureP@ss1");
+
+        isValid.Should().BeTrue();
+        error.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void ValidatePassword_NullInput_ReturnsRequiredError()
+    {
+        var (isValid, error) = ValidationHelper.ValidatePassword(null);
+
+        isValid.Should().BeFalse();
+        error.Should().Be("Password is required");
+    }
+
+    [Fact]
+    public void ValidateEmail_ValidEmail_ReturnsSuccess()
+    {
+        var (isValid, error) = ValidationHelper.ValidateEmail("user@example.com");
+
+        isValid.Should().BeTrue();
+        error.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void ValidateEmail_NoAtSymbol_ReturnsInvalidFormatError()
+    {
+        var (isValid, error) = ValidationHelper.ValidateEmail("notanemail");
+
+        isValid.Should().BeFalse();
+        error.Should().ContainEquivalentOf("invalid");
+    }
+}
