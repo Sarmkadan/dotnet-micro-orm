3c9dbf0048114e68a36ee8881f46d106628d42e0
Vladyslav Zaiets <rutova2@gmail.com>
2021-05-11 11:37:58 +0300

    tweak HealthController.
    also touched a couple of call sites.

--- src/Controllers/HealthController.cs
+++ src/Controllers/HealthController.cs
@@ -6,23 +6,38 @@ using DotnetMicroOrm.Utils;
 
 namespace DotnetMicroOrm.Controllers
 {
+    /// <summary>
+    /// Controller for health checks.
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public sealed class HealthController : ControllerBase
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HealthController"/> class.
+        /// </summary>
+        /// <param name="_dbContext">The database context.</param>
         private readonly IDatabaseContext _dbContext;
 
-        public HealthController(IDatabaseContext dbContext)
+        public HealthController(IDatabaseContext _dbContext)
         {
-            _dbContext = dbContext;
+            this._dbContext = _dbContext;
         }
 
+        /// <summary>
+        /// Returns a success response indicating the service is healthy.
+        /// </summary>
+        /// <returns>A successful HTTP response.</returns>
         [HttpGet]
         public IActionResult GetHealth()
         {
             return Ok(ApiResponse.CreateSuccess(new { status = "healthy" }, "Service is healthy"));
         }
 
+        /// <summary>
+        /// Returns a success or error response indicating the service is ready or not.
+        /// </summary>
+        /// <returns>A successful or error HTTP response.</returns>
         [HttpGet("ready")]
         public async Task<IActionResult> GetReady()
         {
