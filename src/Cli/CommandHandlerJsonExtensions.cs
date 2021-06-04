dd2b9a4fddc9618d3fc58f7e200b4c8eb1f9a237
Vladyslav Zaiets <rutova2@gmail.com>
2021-06-04 15:21:15 +0300

    serialize CommandHandlerJson properly

---
+++ src/Cli/CommandHandlerJsonExtensions.cs
@@ -0,0 +1,89 @@
+#nullable enable
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DotnetMicroOrm.Cli;
+
+/// <summary>
+/// Provides System.Text.Json serialization and deserialization extensions for CommandHandler.
+/// </summary>
+public static class CommandHandlerJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        ReferenceHandler = ReferenceHandler.IgnoreCycles
+    };
+
+
+    /// <summary>
+    /// Serializes a CommandHandler instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The CommandHandler instance to serialize</param>
+    /// <param name="indented">Whether to format the JSON with indentation</param>
+    /// <returns>A JSON string representation of the CommandHandler</returns>
+    public static string ToJson(this CommandHandler value, bool indented = false)
+    {
+        if (value is null)
+        {
+            return "null";
+        }
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonOptions)
+            {
+                WriteIndented = true
+            }
+            : _jsonOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a CommandHandler instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize</param>
+    /// <returns>A CommandHandler instance, or null if the JSON is null or empty</returns>
+    public static CommandHandler? FromJson(string json)
+    {
+        if (string.IsNullOrWhiteSpace(json) || json == "null")
+        {
+            return null;
+        }
+
+        return JsonSerializer.Deserialize<CommandHandler>(json, _jsonOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a CommandHandler instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize</param>
+    /// <param name="value">The resulting CommandHandler instance, or null if deserialization fails</param>
+    /// <returns>True if deserialization succeeded, false otherwise</returns>
+    public static bool TryFromJson(string json, out CommandHandler? value)
+    {
+        value = null;
+
+        if (string.IsNullOrWhiteSpace(json) || json == "null")
+        {
+            return true;
+        }
+
+        try
+        {
+            value = JsonSerializer.Deserialize<CommandHandler>(json, _jsonOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            return false;
+        }
+    }
+}
