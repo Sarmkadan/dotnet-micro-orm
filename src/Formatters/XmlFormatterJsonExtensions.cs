cd485a4aff508c5535c9753ec00645b708bca218
Vladyslav Zaiets <rutova2@gmail.com>
2021-06-18 16:07:17 +0300

    feat: fix

---
+++ src/Formatters/XmlFormatterJsonExtensions.cs
@@ -0,0 +1,72 @@
+#nullable enable
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DotnetMicroOrm.Formatters;
+
+/// <summary>
+/// Provides JSON serialization and deserialization extensions for <see cref="XmlFormatter"/>.
+/// </summary>
+public static class XmlFormatterJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        ReferenceHandler = ReferenceHandler.IgnoreCycles
+    };
+
+    /// <summary>
+    /// Converts the <see cref="XmlFormatter"/> instance to a JSON string representation.
+    /// </summary>
+    /// <param name="value">The <see cref="XmlFormatter"/> instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the <see cref="XmlFormatter"/> instance.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    public static string ToJson(this XmlFormatter value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
+            : _jsonOptions;
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string into an <see cref="XmlFormatter"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>An <see cref="XmlFormatter"/> instance created from the JSON data.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+    public static XmlFormatter? FromJson(string json)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+        return JsonSerializer.Deserialize<XmlFormatter>(json, _jsonOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string into an <see cref="XmlFormatter"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized <see cref="XmlFormatter"/> instance if successful.</param>
+    /// <returns>True if deserialization succeeds; otherwise, false.</returns>
+    public static bool TryFromJson(string json, out XmlFormatter? value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<XmlFormatter>(json, _jsonOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+}
