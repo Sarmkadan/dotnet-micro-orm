a07d45c3d30389dd48ef915b72da79dff0315fa7
Vladyslav Zaiets <rutova2@gmail.com>
2021-07-05 14:50:40 +0300

    StringHelperTestsJson (de)serialization helpers
    will circle back to tests later.

---
+++ tests/dotnet-micro-orm.Tests/StringHelperTestsJsonExtensions.cs
@@ -0,0 +1,92 @@
+#nullable enable
+
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =====================================================================
+
+using System.Text.Json;
+
+namespace DotnetMicroOrm.Tests;
+
+/// <summary>
+/// Provides System.Text.Json serialization extensions for <see cref="StringHelperTests"/> instances.
+/// </summary>
+public static class StringHelperTestsJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        PropertyNameCaseInsensitive = true
+    };
+
+    /// <summary>
+    /// Serializes a <see cref="StringHelperTests"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The instance to serialize</param>
+    /// <param name="indented">Whether to format the JSON with indentation</param>
+    /// <returns>JSON string representation</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null</exception>
+    public static string ToJson(this StringHelperTests value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonSerializerOptions)
+            {
+                WriteIndented = true
+            }
+            : _jsonSerializerOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="StringHelperTests"/> instance.
+    /// </summary>
+    /// <param name="json">JSON string to deserialize</param>
+    /// <returns>Deserialized instance, or null if JSON is null or whitespace</returns>
+    public static StringHelperTests? FromJson(string json)
+    {
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return null;
+        }
+
+        try
+        {
+            return JsonSerializer.Deserialize<StringHelperTests>(json, _jsonSerializerOptions);
+        }
+        catch (JsonException)
+        {
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="StringHelperTests"/> instance.
+    /// </summary>
+    /// <param name="json">JSON string to deserialize</param>
+    /// <param name="value">Output parameter for the deserialized instance</param>
+    /// <returns>True if deserialization succeeded, false otherwise</returns>
+    public static bool TryFromJson(string json, out StringHelperTests? value)
+    {
+        value = null;
+
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return false;
+        }
+
+        try
+        {
+            value = JsonSerializer.Deserialize<StringHelperTests>(json, _jsonSerializerOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            return false;
+        }
+    }
+}
