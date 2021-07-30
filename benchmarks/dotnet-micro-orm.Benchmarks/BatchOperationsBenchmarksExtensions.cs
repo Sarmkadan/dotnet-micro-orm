8b03b7b5888956d3b946a0bb319dd9739d4acaca
Vladyslav Zaiets <rutova2@gmail.com>
2021-07-30 20:04:05 +0300

    A couple of BatchOperationsBenchmarks extensions

--- benchmarks/dotnet-micro-orm.Benchmarks/BatchOperationsBenchmarksExtensions.cs
+++ benchmarks/dotnet-micro-orm.Benchmarks/BatchOperationsBenchmarksExtensions.cs
@@ -1,37 +1,71 @@
 using System;
 using System.Threading.Tasks;
-using DotnetMicroOrm.Benchmarks;
 
-public static class BatchOperationsBenchmarksExtensions
+#nullable enable
+
+namespace DotnetMicroOrm.Benchmarks
 {
-    public static async Task WarmupCache(this BatchOperationsBenchmarks benchmarks)
+    /// <summary>
+    /// Provides extension methods for <see cref="BatchOperationsBenchmarks"/> to simplify benchmark execution and setup.
+    /// </summary>
+    public static class BatchOperationsBenchmarksExtensions
     {
-        await benchmarks.AddRangeAsync_1000_Entities();
-        await benchmarks.AddRangeAsync_5000_Entities();
-    }
+        /// <summary>
+        /// Executes a warmup sequence for batch operations to stabilize measurements.
+        /// This should be called before actual benchmark measurements to account for JIT compilation and cache warming.
+        /// </summary>
+        /// <param name="benchmarks">The <see cref="BatchOperationsBenchmarks"/> instance.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="benchmarks"/> is <see langword="null"/>.</exception>
+        /// <returns>Task representing the warmup operation.</returns>
+        public static async Task WarmupCache(this BatchOperationsBenchmarks benchmarks)
+        {
+            ArgumentNullException.ThrowIfNull(benchmarks);
 
-    public static async Task RunAllBenchmarks(this BatchOperationsBenchmarks benchmarks)
-    {
-        await benchmarks.GlobalSetup();
-        await benchmarks.AddRangeAsync_1000_Entities();
-        await benchmarks.AddRangeAsync_5000_Entities();
-        await benchmarks.UpdateRangeAsync_1000_Entities();
-        await benchmarks.UpdateRangeAsync_5000_Entities();
-        await benchmarks.DeleteRangeAsync_1000_Entities();
-        await benchmarks.DeleteRangeAsync_5000_Entities();
-        await benchmarks.BulkInsert_100_Entities();
-        await benchmarks.BulkInsert_10000_Entities();
-        await benchmarks.BatchInsert_1000_Entities_With_Relations();
-        await benchmarks.BatchUpdate_Complex_Predicate();
-        await benchmarks.BatchDelete_With_Where_Clause();
-        await benchmarks.GlobalCleanup();
-    }
+            await benchmarks.AddRangeAsync_1000_Entities();
+            await benchmarks.AddRangeAsync_5000_Entities();
+        }
 
-    public static async Task MeasureMemoryUsage(this BatchOperationsBenchmarks benchmarks)
-    {
-        var memBefore = GC.GetTotalMemory(true);
-        await benchmarks.AddRangeAsync_1000_Entities();
-        var memAfter = GC.GetTotalMemory(true);
-        Console.WriteLine($"Memory usage: {memAfter - memBefore} bytes");
+        /// <summary>
+        /// Executes all benchmark methods on the provided <see cref="BatchOperationsBenchmarks"/> instance.
+        /// This runs the complete suite of batch operation benchmarks including setup, execution, and cleanup.
+        /// </summary>
+        /// <param name="benchmarks">The <see cref="BatchOperationsBenchmarks"/> instance.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="benchmarks"/> is <see langword="null"/>.</exception>
+        /// <returns>Task representing the complete benchmark run.</returns>
+        public static async Task RunAllBenchmarks(this BatchOperationsBenchmarks benchmarks)
+        {
+            ArgumentNullException.ThrowIfNull(benchmarks);
+
+            await benchmarks.GlobalSetup();
+            await benchmarks.AddRangeAsync_1000_Entities();
+            await benchmarks.AddRangeAsync_5000_Entities();
+            await benchmarks.UpdateRangeAsync_1000_Entities();
+            await benchmarks.UpdateRangeAsync_5000_Entities();
+            await benchmarks.DeleteRangeAsync_1000_Entities();
+            await benchmarks.DeleteRangeAsync_5000_Entities();
+            await benchmarks.BulkInsert_100_Entities();
+            await benchmarks.BulkInsert_10000_Entities();
+            await benchmarks.BatchInsert_1000_Entities_With_Relations();
+            await benchmarks.BatchUpdate_Complex_Predicate();
+            await benchmarks.BatchDelete_With_Where_Clause();
+            await benchmarks.GlobalCleanup();
+        }
+
+        /// <summary>
+        /// Measures memory allocation during batch insert operations by executing a standard batch insert
+        /// and comparing memory usage before and after the operation.
+        /// </summary>
+        /// <param name="benchmarks">The <see cref="BatchOperationsBenchmarks"/> instance.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="benchmarks"/> is <see langword="null"/>.</exception>
+        /// <returns>Task representing the memory measurement operation.</returns>
+        public static async Task MeasureMemoryUsage(this BatchOperationsBenchmarks benchmarks)
+        {
+            ArgumentNullException.ThrowIfNull(benchmarks);
+
+            var memBefore = GC.GetTotalMemory(true);
+            await benchmarks.AddRangeAsync_1000_Entities();
+            var memAfter = GC.GetTotalMemory(true);
+            Console.WriteLine($"Memory usage: {memAfter - memBefore} bytes");
+        }
     }
 }
