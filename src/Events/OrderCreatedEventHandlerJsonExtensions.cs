00369bebe2a26aabf0e10fba957664c96aedabe4
Vladyslav Zaiets <rutova2@gmail.com>
2021-08-02 16:12:03 +0300

    Add OrderCreatedEventHandlerJson json helpers

--- src/Events/OrderCreatedEventHandlerJsonExtensions.cs
+++ src/Events/OrderCreatedEventHandlerJsonExtensions.cs
@@ -5,16 +5,17 @@
 // CTO & Software Architect
 // =====================================================================
 
+using System.Diagnostics.CodeAnalysis;
 using System.Text.Json;
 
 namespace DotnetMicroOrm.Events;
 
 /// <summary>
-/// Provides System.Text.Json serialization extensions for OrderCreatedEventHandler
+/// Provides System.Text.Json serialization extensions for <see cref="OrderCreatedEventHandler"/>.
 /// </summary>
 public static class OrderCreatedEventHandlerJsonExtensions
 {
-    private static readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new()
     {
         PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
         WriteIndented = false,
@@ -22,33 +23,25 @@ public static class OrderCreatedEventHandlerJsonExtensions
     };
 
     /// <summary>
-    /// Serializes the OrderCreatedEventHandler instance to a JSON string
+    /// Serializes the <see cref="OrderCreatedEventHandler"/> instance to a JSON string.
     /// </summary>
-    /// <param name="value">The handler instance to serialize</param>
-    /// <param name="indented">Whether to format the JSON with indentation</param>
-    /// <returns>JSON string representation</returns>
-    public static string ToJson(this OrderCreatedEventHandler value, bool indented = false)
-    {
-        if (value is null)
-        {
-            throw new ArgumentNullException(nameof(value));
-        }
-
-        var options = indented
-            ? new JsonSerializerOptions(_jsonSerializerOptions)
-            {
-                WriteIndented = true
-            }
-            : _jsonSerializerOptions;
-
-        return JsonSerializer.Serialize(value, options);
-    }
+    /// <param name="value">The handler instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation.</param>
+    /// <returns>JSON string representation.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <see langword="null"/>.</exception>
+    public static string ToJson(this OrderCreatedEventHandler value, bool indented = false) =>
+        value is null
+            ? throw new ArgumentNullException(nameof(value))
+            : JsonSerializer.Serialize(value, indented
+                ? new JsonSerializerOptions(_jsonSerializerOptions) { WriteIndented = true }
+                : _jsonSerializerOptions);
 
     /// <summary>
-    /// Deserializes a JSON string to an OrderCreatedEventHandler instance
+    /// Deserializes a JSON string to an <see cref="OrderCreatedEventHandler"/> instance.
     /// </summary>
-    /// <param name="json">JSON string to deserialize</param>
-    /// <returns>Deserialized handler instance, or null if JSON is null or empty</returns>
+    /// <param name="json">JSON string to deserialize.</param>
+    /// <returns>Deserialized handler instance, or <see langword="null"/> if JSON is <see langword="null"/>, whitespace, or deserialization fails.</returns>
+    /// <exception cref="JsonException">Thrown when JSON is valid but cannot be deserialized to <see cref="OrderCreatedEventHandler"/>.</exception>
     public static OrderCreatedEventHandler? FromJson(string json)
     {
         if (string.IsNullOrWhiteSpace(json))
@@ -67,27 +60,28 @@ public static class OrderCreatedEventHandlerJsonExtensions
     }
 
     /// <summary>
-    /// Attempts to deserialize a JSON string to an OrderCreatedEventHandler instance
+    /// Attempts to deserialize a JSON string to an <see cref="OrderCreatedEventHandler"/> instance.
     /// </summary>
-    /// <param name="json">JSON string to deserialize</param>
-    /// <param name="value">Output parameter for the deserialized handler</param>
-    /// <returns>True if deserialization succeeded, false otherwise</returns>
-    public static bool TryFromJson(string json, out OrderCreatedEventHandler? value)
+    /// <param name="json">JSON string to deserialize.</param>
+    /// <param name="value">Output parameter for the deserialized handler.</param>
+    /// <returns><see langword="true"/> if deserialization succeeded; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is <see langword="null"/>.</exception>
+    public static bool TryFromJson(string json, [NotNullWhen(true)] out OrderCreatedEventHandler? value)
     {
-        value = null;
-
         if (string.IsNullOrWhiteSpace(json))
         {
+            value = null;
             return false;
         }
 
         try
         {
             value = JsonSerializer.Deserialize<OrderCreatedEventHandler>(json, _jsonSerializerOptions);
-            return true;
+            return value is not null;
         }
         catch (JsonException)
         {
+            value = null;
             return false;
         }
     }
