5703cf8b52319ae87c8bc3f51b4ced8cc6ef03a8
Vladyslav Zaiets <rutova2@gmail.com>
2021-08-05 17:10:36 +0300

    TableAttributeJson (de)serialization helpers

--- src/Domain/Models/TableAttributeJsonExtensions.cs
+++ src/Domain/Models/TableAttributeJsonExtensions.cs
@@ -15,81 +15,83 @@ namespace DotnetMicroOrm.Domain.Models;
 /// </summary>
 public static class TableAttributeJsonExtensions
 {
-    private static readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions
-    {
-        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-        WriteIndented = false,
-        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
-    };
+	private static readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions
+	{
+		PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+		WriteIndented = false,
+		DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+	};
 
-    /// <summary>
-    /// Serializes a TableAttribute to a JSON string
-    /// </summary>
-    /// <param name="value">The TableAttribute to serialize</param>
-    /// <param name="indented">Whether to format the JSON with indentation</param>
-    /// <returns>A JSON string representation of the TableAttribute</returns>
-    public static string ToJson(this TableAttribute value, bool indented = false)
-    {
-        if (value is null)
-        {
-            return "null";
-        }
+	/// <summary>
+	/// Serializes a TableAttribute to a JSON string
+	/// </summary>
+	/// <param name="value">The TableAttribute to serialize</param>
+	/// <param name="indented">Whether to format the JSON with indentation</param>
+	/// <returns>A JSON string representation of the TableAttribute</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+	public static string ToJson(this TableAttribute value, bool indented = false)
+	{
+		ArgumentNullException.ThrowIfNull(value);
 
-        var options = indented
-            ? new JsonSerializerOptions(_jsonSerializerOptions)
-            {
-                WriteIndented = true
-            }
-            : _jsonSerializerOptions;
+		var options = indented
+			? new JsonSerializerOptions(_jsonSerializerOptions)
+				{ WriteIndented = true }
+				: _jsonSerializerOptions;
 
-        return JsonSerializer.Serialize(value, options);
-    }
+		return JsonSerializer.Serialize(value, options);
+	}
 
-    /// <summary>
-    /// Deserializes a TableAttribute from a JSON string
-    /// </summary>
-    /// <param name="json">The JSON string to deserialize</param>
-    /// <returns>The deserialized TableAttribute, or null if JSON is null or empty</returns>
-    public static TableAttribute? FromJson(string json)
-    {
-        if (string.IsNullOrWhiteSpace(json) || json == "null")
-        {
-            return null;
-        }
+	/// <summary>
+	/// Deserializes a TableAttribute from a JSON string
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize</param>
+	/// <returns>The deserialized TableAttribute, or null if JSON is null or empty</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+	public static TableAttribute? FromJson(string json)
+	{
+		ArgumentNullException.ThrowIfNull(json);
 
-        try
-        {
-            return JsonSerializer.Deserialize<TableAttribute>(json, _jsonSerializerOptions);
-        }
-        catch (JsonException)
-        {
-            return null;
-        }
-    }
+		if (string.IsNullOrWhiteSpace(json) || json == "null")
+		{
+			return null;
+		}
 
-    /// <summary>
-    /// Attempts to deserialize a TableAttribute from a JSON string
-    /// </summary>
-    /// <param name="json">The JSON string to deserialize</param>
-    /// <param name="value">The deserialized TableAttribute, or null if deserialization fails</param>
-    /// <returns>True if deserialization succeeded; otherwise, false</returns>
-    public static bool TryFromJson(string json, out TableAttribute? value)
-    {
-        value = null;
+		try
+		{
+			return JsonSerializer.Deserialize<TableAttribute>(json, _jsonSerializerOptions);
+		}
+		catch (JsonException)
+		{
+			return null;
+		}
+	}
 
-        if (string.IsNullOrWhiteSpace(json) || json == "null")
-        {
-            return false;
-        }
+	/// <summary>
+	/// Attempts to deserialize a TableAttribute from a JSON string
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize</param>
+	/// <param name="value">The deserialized TableAttribute, or null if deserialization fails</param>
+	/// <returns>True if deserialization succeeded; otherwise, false</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+	public static bool TryFromJson(string json, out TableAttribute? value)
+	{
+		value = null;
 
-        try
-        {
-            value = JsonSerializer.Deserialize<TableAttribute>(json, _jsonSerializerOptions);
-            return true;
-        }
-        catch (JsonException)
-        {
-            return false;
-        }
-    }
+		ArgumentNullException.ThrowIfNull(json);
+
+		if (string.IsNullOrWhiteSpace(json) || json == "null")
+		{
+			return false;
+		}
+
+		try
+		{
+			value = JsonSerializer.Deserialize<TableAttribute>(json, _jsonSerializerOptions);
+			return true;
+		}
+		catch (JsonException)
+		{
+			return false;
+		}
+	}
 }
