3c87740223c656cb2e8ffc0cf306f1e75d095bba
Vladyslav Zaiets <rutova2@gmail.com>
2021-08-06 10:53:24 +0300

    Json round-trip support for OrderServiceJson

--- src/Services/OrderServiceJsonExtensions.cs
+++ src/Services/OrderServiceJsonExtensions.cs
@@ -7,11 +7,10 @@
 namespace DotnetMicroOrm.Services;
 
 using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Text.Json.Serialization.Metadata;
+using JsonException = System.Text.Json.JsonException;
 
 /// <summary>
-/// Provides System.Text.Json serialization extensions for OrderService
+/// Provides System.Text.Json serialization extensions for <see cref="OrderService"/>
 /// </summary>
 public static class OrderServiceJsonExtensions
 {
@@ -19,22 +18,19 @@ public static class OrderServiceJsonExtensions
     {
         PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
         WriteIndented = false,
-        TypeInfoResolver = new DefaultJsonTypeInfoResolver(),
-        ReferenceHandler = ReferenceHandler.IgnoreCycles
+        ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles
     };
 
     /// <summary>
-    /// Serializes OrderService instance to JSON string
+    /// Serializes <see cref="OrderService"/> instance to JSON string
     /// </summary>
     /// <param name="value">OrderService instance to serialize</param>
     /// <param name="indented">Whether to format the JSON with indentation</param>
     /// <returns>JSON string representation</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/></exception>
     public static string ToJson(this OrderService value, bool indented = false)
     {
-        if (value is null)
-        {
-            return "{}";
-        }
+        ArgumentNullException.ThrowIfNull(value);
 
         var options = indented
             ? new JsonSerializerOptions(_jsonOptions)
@@ -47,12 +43,15 @@ public static class OrderServiceJsonExtensions
     }
 
     /// <summary>
-    /// Deserializes JSON string to OrderService instance
+    /// Deserializes JSON string to <see cref="OrderService"/> instance
     /// </summary>
     /// <param name="json">JSON string to deserialize</param>
-    /// <returns>Deserialized OrderService instance or null if JSON is invalid</returns>
+    /// <returns>Deserialized OrderService instance or <see langword="null"/> if JSON is invalid</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/></exception>
     public static OrderService? FromJson(string json)
     {
+        ArgumentNullException.ThrowIfNull(json);
+
         if (string.IsNullOrWhiteSpace(json))
         {
             return null;
@@ -69,27 +68,30 @@ public static class OrderServiceJsonExtensions
     }
 
     /// <summary>
-    /// Attempts to deserialize JSON string to OrderService instance
+    /// Attempts to deserialize JSON string to <see cref="OrderService"/> instance
     /// </summary>
     /// <param name="json">JSON string to deserialize</param>
     /// <param name="value">Output parameter for deserialized OrderService</param>
-    /// <returns>True if deserialization succeeded, false otherwise</returns>
-    public static bool TryFromJson(string json, out OrderService? value)
+    /// <returns><see langword="true"/> if deserialization succeeded; otherwise, <see langword="false"/></returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/></exception>
+    public static bool TryFromJson(string json, [System.Diagnostics.CodeAnalysis.NotNullWhen(true)] out OrderService? value)
     {
-        value = null;
+        ArgumentNullException.ThrowIfNull(json);
 
         if (string.IsNullOrWhiteSpace(json))
         {
+            value = null;
             return false;
         }
 
         try
         {
             value = JsonSerializer.Deserialize<OrderService>(json, _jsonOptions);
-            return true;
+            return value is not null;
         }
         catch (JsonException)
         {
+            value = null;
             return false;
         }
     }
