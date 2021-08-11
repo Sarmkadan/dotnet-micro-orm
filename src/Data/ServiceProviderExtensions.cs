dae0a7d13dfe4f893150c90faae85fe2887c5949
Vladyslav Zaiets <rutova2@gmail.com>
2021-08-11 12:26:29 +0300

    feat: more ServiceProvider extension methods

--- src/Data/ServiceProviderExtensions.cs
+++ src/Data/ServiceProviderExtensions.cs
@@ -5,36 +5,37 @@ using System;
 namespace DotnetMicroOrm.Data;
 
 /// <summary>
-/// Extension methods for IServiceProvider to provide convenient service resolution
+/// Extension methods for <see cref="IServiceProvider"/> to provide convenient service resolution
+/// and type-safe service retrieval.
 /// </summary>
 public static class ServiceProviderExtensions
 {
     /// <summary>
-    /// Gets the requested service from the service provider
+    /// Gets the requested service from the service provider.
     /// </summary>
-    /// <typeparam name="T">Service type</typeparam>
-    /// <param name="serviceProvider">Service provider</param>
-    /// <returns>Requested service instance</returns>
-    /// <exception cref="InvalidOperationException">Thrown when service cannot be resolved</exception>
+    /// <typeparam name="T">The service type to resolve.</typeparam>
+    /// <param name="serviceProvider">The service provider instance.</param>
+    /// <returns>The requested service instance.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="serviceProvider"/> is <see langword="null"/></exception>
+    /// <exception cref="InvalidOperationException">The requested service could not be resolved.</exception>
     public static T GetRequiredService<T>(this IServiceProvider serviceProvider)
     {
-        if (serviceProvider == null)
-            throw new ArgumentNullException(nameof(serviceProvider));
+        ArgumentNullException.ThrowIfNull(serviceProvider);
 
         return (T)serviceProvider.GetService(typeof(T))
             ?? throw new InvalidOperationException($"Required service {typeof(T).FullName} not found in service provider");
     }
 
     /// <summary>
-    /// Gets the requested service from the service provider or returns null
+    /// Gets the requested service from the service provider or returns null if not found.
     /// </summary>
-    /// <typeparam name="T">Service type</typeparam>
-    /// <param name="serviceProvider">Service provider</param>
-    /// <returns>Requested service instance or null</returns>
+    /// <typeparam name="T">The service type to resolve.</typeparam>
+    /// <param name="serviceProvider">The service provider instance.</param>
+    /// <returns>The requested service instance, or <see langword="null"/> if not found.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="serviceProvider"/> is <see langword="null"/></exception>
     public static T? GetService<T>(this IServiceProvider serviceProvider)
     {
-        if (serviceProvider == null)
-            throw new ArgumentNullException(nameof(serviceProvider));
+        ArgumentNullException.ThrowIfNull(serviceProvider);
 
         return (T?)serviceProvider.GetService(typeof(T));
     }
