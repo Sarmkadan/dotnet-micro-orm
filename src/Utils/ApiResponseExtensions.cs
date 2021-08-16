999e717d1a535cf6a07fbcb38bed2e0c70706901
Vladyslav Zaiets <rutova2@gmail.com>
2021-08-16 13:46:06 +0300

    Small ApiResponse helpers

--- src/Utils/ApiResponseExtensions.cs
+++ src/Utils/ApiResponseExtensions.cs
@@ -3,7 +3,7 @@
 // =============================================================================
 // Author: Vladyslav Zaiets | https://sarmkadan.com
 // CTO & Software Architect
-// =============================================================================
+// =====================================================================
 
 using System.Text.Json;
 
@@ -21,9 +21,12 @@ public static class ApiResponseExtensions
     /// <param name="response">The API response</param>
     /// <param name="requestId">The request ID to add</param>
     /// <returns>The response with the request ID set</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="response"/> is <see langword="null"/></exception>
+    /// <exception cref="ArgumentException"><paramref name="requestId"/> is <see langword="null"/>, empty, or consists only of whitespace</exception>
     public static ApiResponse WithRequestId(this ApiResponse response, string requestId)
     {
         ArgumentNullException.ThrowIfNull(response);
+        ArgumentException.ThrowIfNullOrWhiteSpace(requestId);
         response.RequestId = requestId;
         return response;
     }
@@ -35,9 +38,12 @@ public static class ApiResponseExtensions
     /// <param name="response">The API response</param>
     /// <param name="requestId">The request ID to add</param>
     /// <returns>The response with the request ID set</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="response"/> is <see langword="null"/></exception>
+    /// <exception cref="ArgumentException"><paramref name="requestId"/> is <see langword="null"/>, empty, or consists only of whitespace</exception>
     public static ApiResponse<T> WithRequestId<T>(this ApiResponse<T> response, string requestId)
     {
         ArgumentNullException.ThrowIfNull(response);
+        ArgumentException.ThrowIfNullOrWhiteSpace(requestId);
         response.RequestId = requestId;
         return response;
     }
@@ -49,9 +55,12 @@ public static class ApiResponseExtensions
     /// <param name="response">The paginated API response</param>
     /// <param name="requestId">The request ID to add</param>
     /// <returns>The response with the request ID set</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="response"/> is <see langword="null"/></exception>
+    /// <exception cref="ArgumentException"><paramref name="requestId"/> is <see langword="null"/>, empty, or consists only of whitespace</exception>
     public static ApiPagedResponse<T> WithRequestId<T>(this ApiPagedResponse<T> response, string requestId)
     {
         ArgumentNullException.ThrowIfNull(response);
+        ArgumentException.ThrowIfNullOrWhiteSpace(requestId);
         response.RequestId = requestId;
         return response;
     }
@@ -62,6 +71,8 @@ public static class ApiResponseExtensions
     /// <param name="response">The API response</param>
     /// <param name="additionalContext">Additional context to append to the message</param>
     /// <returns>The response with updated message</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="response"/> is <see langword="null"/></exception>
+    /// <exception cref="ArgumentException"><paramref name="additionalContext"/> is <see langword="null"/>, empty, or consists only of whitespace</exception>
     public static ApiResponse WithContext(this ApiResponse response, string additionalContext)
     {
         ArgumentNullException.ThrowIfNull(response);
@@ -77,6 +88,8 @@ public static class ApiResponseExtensions
     /// <param name="response">The API response</param>
     /// <param name="additionalContext">Additional context to append to the message</param>
     /// <returns>The response with updated message</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="response"/> is <see langword="null"/></exception>
+    /// <exception cref="ArgumentException"><paramref name="additionalContext"/> is <see langword="null"/>, empty, or consists only of whitespace</exception>
     public static ApiResponse<T> WithContext<T>(this ApiResponse<T> response, string additionalContext)
     {
         ArgumentNullException.ThrowIfNull(response);
@@ -92,6 +105,8 @@ public static class ApiResponseExtensions
     /// <param name="response">The paginated API response</param>
     /// <param name="additionalContext">Additional context to append to the message</param>
     /// <returns>The response with updated message</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="response"/> is <see langword="null"/></exception>
+    /// <exception cref="ArgumentException"><paramref name="additionalContext"/> is <see langword="null"/>, empty, or consists only of whitespace</exception>
     public static ApiPagedResponse<T> WithContext<T>(this ApiPagedResponse<T> response, string additionalContext)
     {
         ArgumentNullException.ThrowIfNull(response);
@@ -106,6 +121,7 @@ public static class ApiResponseExtensions
     /// <param name="response">The API response</param>
     /// <param name="options">Optional JSON serialization options</param>
     /// <returns>JSON string representation of the response</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="response"/> is <see langword="null"/></exception>
     public static string ToJson(this ApiResponse response, JsonSerializerOptions? options = null)
     {
         ArgumentNullException.ThrowIfNull(response);
@@ -119,6 +135,7 @@ public static class ApiResponseExtensions
     /// <param name="response">The API response</param>
     /// <param name="options">Optional JSON serialization options</param>
     /// <returns>JSON string representation of the response</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="response"/> is <see langword="null"/></exception>
     public static string ToJson<T>(this ApiResponse<T> response, JsonSerializerOptions? options = null)
     {
         ArgumentNullException.ThrowIfNull(response);
@@ -132,6 +149,7 @@ public static class ApiResponseExtensions
     /// <param name="response">The paginated API response</param>
     /// <param name="options">Optional JSON serialization options</param>
     /// <returns>JSON string representation of the response</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="response"/> is <see langword="null"/></exception>
     public static string ToJson<T>(this ApiPagedResponse<T> response, JsonSerializerOptions? options = null)
     {
         ArgumentNullException.ThrowIfNull(response);
@@ -145,9 +163,12 @@ public static class ApiResponseExtensions
     /// <param name="response">The original response to copy from</param>
     /// <param name="newMessage">The new success message</param>
     /// <returns>A new success response</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="response"/> is <see langword="null"/></exception>
+    /// <exception cref="ArgumentException"><paramref name="newMessage"/> is <see langword="null"/> or empty</exception>
     public static ApiResponse ToSuccessResponse(this ApiResponse response, string newMessage)
     {
         ArgumentNullException.ThrowIfNull(response);
+        ArgumentException.ThrowIfNullOrWhiteSpace(newMessage);
         return new ApiResponse
         {
             Success = true,
@@ -167,9 +188,12 @@ public static class ApiResponseExtensions
     /// <param name="newData">The new data to include</param>
     /// <param name="newMessage">The new success message</param>
     /// <returns>A new success response with data</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="response"/> is <see langword="null"/></exception>
+    /// <exception cref="ArgumentException"><paramref name="newMessage"/> is <see langword="null"/> or empty</exception>
     public static ApiResponse<T> ToSuccessResponse<T>(this ApiResponse<T> response, T newData, string newMessage)
     {
         ArgumentNullException.ThrowIfNull(response);
+        ArgumentException.ThrowIfNullOrWhiteSpace(newMessage);
         return new ApiResponse<T>
         {
             Success = true,
@@ -189,9 +213,12 @@ public static class ApiResponseExtensions
     /// <param name="errorMessage">The new error message</param>
     /// <param name="errorCode">The new error code</param>
     /// <returns>A new error response</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="response"/> is <see langword="null"/></exception>
+    /// <exception cref="ArgumentException"><paramref name="errorMessage"/> is <see langword="null"/> or empty</exception>
     public static ApiResponse ToErrorResponse(this ApiResponse response, string errorMessage, string errorCode = "ERROR")
     {
         ArgumentNullException.ThrowIfNull(response);
+        ArgumentException.ThrowIfNullOrWhiteSpace(errorMessage);
         return new ApiResponse
         {
             Success = false,
@@ -211,9 +238,12 @@ public static class ApiResponseExtensions
     /// <param name="errorMessage">The new error message</param>
     /// <param name="errorCode">The new error code</param>
     /// <returns>A new error response with default data</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="response"/> is <see langword="null"/></exception>
+    /// <exception cref="ArgumentException"><paramref name="errorMessage"/> is <see langword="null"/> or empty</exception>
     public static ApiResponse<T> ToErrorResponse<T>(this ApiResponse<T> response, string errorMessage, string errorCode = "ERROR")
     {
         ArgumentNullException.ThrowIfNull(response);
+        ArgumentException.ThrowIfNullOrWhiteSpace(errorMessage);
         return new ApiResponse<T>
         {
             Success = false,
@@ -232,6 +262,7 @@ public static class ApiResponseExtensions
     /// <param name="response">The original response</param>
     /// <param name="pagination">Pagination metadata to add</param>
     /// <returns>A new paginated response with the original data</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="response"/> or <paramref name="pagination"/> is <see langword="null"/></exception>
     public static ApiPagedResponse<T> ToPagedResponse<T>(this ApiResponse<T> response, PaginationMetadata pagination)
     {
         ArgumentNullException.ThrowIfNull(response);
@@ -255,6 +286,7 @@ public static class ApiResponseExtensions
     /// <typeparam name="T">Response data type</typeparam>
     /// <param name="response">The paginated response</param>
     /// <returns>List of items from the paginated response</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="response"/> is <see langword="null"/></exception>
     public static List<T> ToItemList<T>(this ApiPagedResponse<T> response)
     {
         ArgumentNullException.ThrowIfNull(response);
@@ -267,6 +299,7 @@ public static class ApiResponseExtensions
     /// <typeparam name="T">Response data type</typeparam>
     /// <param name="response">The paginated response</param>
     /// <returns>True if there are more items to fetch</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="response"/> is <see langword="null"/></exception>
     public static bool HasMoreItems<T>(this ApiPagedResponse<T> response)
     {
         ArgumentNullException.ThrowIfNull(response);
@@ -279,6 +312,7 @@ public static class ApiResponseExtensions
     /// <typeparam name="T">Response data type</typeparam>
     /// <param name="response">The paginated response</param>
     /// <returns>True if this is the first page</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="response"/> is <see langword="null"/></exception>
     public static bool IsFirstPage<T>(this ApiPagedResponse<T> response)
     {
         ArgumentNullException.ThrowIfNull(response);
@@ -291,6 +325,7 @@ public static class ApiResponseExtensions
     /// <typeparam name="T">Response data type</typeparam>
     /// <param name="response">The paginated response</param>
     /// <returns>True if this is the last page</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="response"/> is <see langword="null"/></exception>
     public static bool IsLastPage<T>(this ApiPagedResponse<T> response)
     {
         ArgumentNullException.ThrowIfNull(response);
