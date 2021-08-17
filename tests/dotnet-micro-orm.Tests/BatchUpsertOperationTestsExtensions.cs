84f61c22fca25dfc79cc9f536f6b302b7bff4f40
Vladyslav Zaiets <rutova2@gmail.com>
2021-08-17 16:53:04 +0300

    small BatchUpsertOperation helpers

--- tests/dotnet-micro-orm.Tests/BatchUpsertOperationTestsExtensions.cs
+++ tests/dotnet-micro-orm.Tests/BatchUpsertOperationTestsExtensions.cs
@@ -15,14 +15,12 @@ public static class BatchUpsertOperationTestsExtensions
     /// Gets only the entities that were inserted during the upsert operation.
     /// </summary>
     /// <typeparam name="T">The entity type.</typeparam>
-    /// <param name="results">The upsert results from the operation.</param>
-    /// <returns>A list of entities that were inserted.</returns>
+    /// <param name="results">The upsert results from the operation. Cannot be <see langword="null"/>.</param>
+    /// <returns>A list of entities that were inserted. Never <see langword="null"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="results"/> is <see langword="null"/>.</exception>
     public static List<T> GetInsertedEntities<T>(this List<UpsertResult<T>> results) where T : BaseEntity
     {
-        if (results is null)
-        {
-            throw new ArgumentNullException(nameof(results));
-        }
+        ArgumentNullException.ThrowIfNull(results);
 
         return results
             .Where(r => r.WasInserted)
@@ -34,14 +32,12 @@ public static class BatchUpsertOperationTestsExtensions
     /// Gets only the entities that were updated during the upsert operation.
     /// </summary>
     /// <typeparam name="T">The entity type.</typeparam>
-    /// <param name="results">The upsert results from the operation.</param>
-    /// <returns>A list of entities that were updated.</returns>
+    /// <param name="results">The upsert results from the operation. Cannot be <see langword="null"/>.</param>
+    /// <returns>A list of entities that were updated. Never <see langword="null"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="results"/> is <see langword="null"/>.</exception>
     public static List<T> GetUpdatedEntities<T>(this List<UpsertResult<T>> results) where T : BaseEntity
     {
-        if (results is null)
-        {
-            throw new ArgumentNullException(nameof(results));
-        }
+        ArgumentNullException.ThrowIfNull(results);
 
         return results
             .Where(r => !r.WasInserted)
@@ -53,22 +49,18 @@ public static class BatchUpsertOperationTestsExtensions
     /// Filters the upsert results to only include entities matching the specified predicate.
     /// </summary>
     /// <typeparam name="T">The entity type.</typeparam>
-    /// <param name="results">The upsert results from the operation.</param>
-    /// <param name="predicate">The filter predicate to apply to the entities.</param>
-    /// <returns>A filtered list of upsert results.</returns>
+    /// <param name="results">The upsert results from the operation. Cannot be <see langword="null"/>.</param>
+    /// <param name="predicate">The filter predicate to apply to the entities. Cannot be <see langword="null"/>.</param>
+    /// <returns>A filtered list of upsert results. Never <see langword="null"/>.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// <paramref name="results"/> or <paramref name="predicate"/> is <see langword="null"/>.
+    /// </exception>
     public static List<UpsertResult<T>> WhereEntity<T>(
         this List<UpsertResult<T>> results,
         Func<T, bool> predicate) where T : BaseEntity
     {
-        if (results is null)
-        {
-            throw new ArgumentNullException(nameof(results));
-        }
-
-        if (predicate is null)
-        {
-            throw new ArgumentNullException(nameof(predicate));
-        }
+        ArgumentNullException.ThrowIfNull(results);
+        ArgumentNullException.ThrowIfNull(predicate);
 
         return results
             .Where(r => predicate(r.Entity))
@@ -79,14 +71,12 @@ public static class BatchUpsertOperationTestsExtensions
     /// Checks if any of the upsert operations resulted in an insert.
     /// </summary>
     /// <typeparam name="T">The entity type.</typeparam>
-    /// <param name="results">The upsert results from the operation.</param>
-    /// <returns>True if any entity was inserted; otherwise false.</returns>
+    /// <param name="results">The upsert results from the operation. Cannot be <see langword="null"/>.</param>
+    /// <returns><see langword="true"/> if any entity was inserted; otherwise <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="results"/> is <see langword="null"/>.</exception>
     public static bool AnyInserted<T>(this List<UpsertResult<T>> results) where T : BaseEntity
     {
-        if (results is null)
-        {
-            throw new ArgumentNullException(nameof(results));
-        }
+        ArgumentNullException.ThrowIfNull(results);
 
         return results.Any(r => r.WasInserted);
     }
