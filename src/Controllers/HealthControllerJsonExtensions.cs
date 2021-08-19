7edf4a081e3833560b4ea3aedea2f6b8ff969a04
Vladyslav Zaiets <rutova2@gmail.com>
2021-08-19 12:32:29 +0300

    Json round-trip support for HealthControllerJson

--- src/Controllers/HealthControllerJsonExtensions.cs
+++ src/Controllers/HealthControllerJsonExtensions.cs
@@ -4,23 +4,24 @@ using System.Text.Json.Serialization;
 namespace DotNetMicroORM.Controllers;
 
 /// <summary>
-/// Provides JSON serialization and deserialization extensions for <see cref="HealthController"/>.
+/// Provides JSON serialization and deserialization extension methods for <see cref="HealthController"/>.
 /// </summary>
 public static class HealthControllerJsonExtensions
 {
-	private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
-	{
-		PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-		WriteIndented = false,
-		DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
-	};
-
 	/// <summary>
-	/// Serializes a <see cref="HealthController"/> instance to a JSON string.
+	/// Serializes a <see cref="HealthController"/> instance to a JSON string using the predefined
+	/// <see cref="_jsonOptions"/>. If <paramref name="value"/> is <c>null</c>, the method returns an
+	/// empty JSON object (<c>{}</c>).
 	/// </summary>
-	/// <param name="value">The <see cref="HealthController"/> instance to serialize. Can be <see langword="null"/>.</param>
-	/// <param name="indented">Whether to format the JSON with indentation for readability.</param>
-	/// <returns>A JSON string representation of the <see cref="HealthController"/>. Returns "{}" if <paramref name="value"/> is <see langword="null"/>.</returns>
+	/// <param name="value">The <see cref="HealthController"/> instance to serialize. May be <c>null</c>.</param>
+	/// <param name="indented">
+	/// When <c>true</c>, the output JSON is formatted with indentation for readability; otherwise,
+	/// the JSON is written in a compact form.
+	/// </param>
+	/// <returns>
+	/// A JSON string representation of <paramref name="value"/>. Returns <c>"{}"</c> when
+	/// <paramref name="value"/> is <c>null</c>.
+	/// </returns>
 	public static string ToJson(this HealthController? value, bool indented = false)
 	{
 		if (value is null)
@@ -39,11 +40,17 @@ public static class HealthControllerJsonExtensions
 	}
 
 	/// <summary>
-	/// Deserializes a JSON string to a <see cref="HealthController"/> instance.
+	/// Deserializes a JSON string into a <see cref="HealthController"/> instance using the predefined
+	/// <see cref="_jsonOptions"/>.
 	/// </summary>
-	/// <param name="json">The JSON string to deserialize. Cannot be <see langword="null"/> or empty.</param>
-	/// <returns>The deserialized <see cref="HealthController"/> instance, or <see langword="null"/> if deserialization fails.</returns>
-	/// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/>.</exception>
+	/// <param name="json">
+	/// The JSON string to deserialize. Must not be <c>null</c> or consist only of whitespace.
+	/// </param>
+	/// <returns>
+	/// The deserialized <see cref="HealthController"/> instance, or <c>null</c> if deserialization fails
+	/// due to a <see cref="JsonException"/>.
+	/// </returns>
+	/// <exception cref="ArgumentNullException"><paramref name="json"/> is <c>null</c>.</exception>
 	/// <exception cref="ArgumentException"><paramref name="json"/> is empty or whitespace.</exception>
 	public static HealthController? FromJson(string json)
 	{
@@ -61,12 +68,20 @@ public static class HealthControllerJsonExtensions
 	}
 
 	/// <summary>
-	/// Attempts to deserialize a JSON string to a <see cref="HealthController"/> instance.
+	/// Attempts to deserialize a JSON string into a <see cref="HealthController"/> instance without
+	/// propagating any <see cref="JsonException"/> that may occur.
 	/// </summary>
-	/// <param name="json">The JSON string to deserialize. Cannot be <see langword="null"/> or empty.</param>
-	/// <param name="value">Receives the deserialized <see cref="HealthController"/> instance if successful; otherwise, <see langword="null"/>.</param>
-	/// <returns><see langword="true"/> if deserialization succeeds; otherwise, <see langword="false"/>.</returns>
-	/// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/>.</exception>
+	/// <param name="json">
+	/// The JSON string to deserialize. Must not be <c>null</c> or consist only of whitespace.
+	/// </param>
+	/// <param name="value">
+	/// When the method returns <c>true</c>, receives the deserialized <see cref="HealthController"/>
+	/// instance; otherwise, receives <c>null</c>.
+	/// </param>
+	/// <returns>
+	/// <c>true</c> if deserialization succeeds; otherwise, <c>false</c>.
+	/// </returns>
+	/// <exception cref="ArgumentNullException"><paramref name="json"/> is <c>null</c>.</exception>
 	/// <exception cref="ArgumentException"><paramref name="json"/> is empty or whitespace.</exception>
 	public static bool TryFromJson(string json, out HealthController? value)
 	{
