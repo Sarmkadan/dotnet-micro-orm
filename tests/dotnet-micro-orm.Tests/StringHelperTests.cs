f6b9dab69c82741b6c4ca7e6d18bbe21f3ca8c14
Vladyslav Zaiets <rutova2@gmail.com>
2026-02-23 14:19:42 +0000

    Add unit tests

---
+++ tests/dotnet-micro-orm.Tests/StringHelperTests.cs
@@ -0,0 +1,45 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using DotnetMicroOrm.Utils;
+using FluentAssertions;
+
+namespace DotnetMicroOrm.Tests;
+
+public class StringHelperTests
+{
+    [Fact]
+    public void ToKebabCase_PascalCaseInput_ReturnsKebabCase()
+    {
+        var result = StringHelper.ToKebabCase("UserProfile");
+
+        result.Should().Be("user-profile");
+    }
+
+    [Fact]
+    public void ToSnakeCase_PascalCaseInput_ReturnsSnakeCase()
+    {
+        var result = StringHelper.ToSnakeCase("UserProfile");
+
+        result.Should().Be("user_profile");
+    }
+
+    [Fact]
+    public void ToPascalCase_KebabCaseInput_ReturnsPascalCase()
+    {
+        var result = StringHelper.ToPascalCase("user-profile");
+
+        result.Should().Be("UserProfile");
+    }
+
+    [Fact]
+    public void Truncate_StringExceedsMaxLength_AppendsSuffixAndRespectsBound()
+    {
+        var result = StringHelper.Truncate("Hello World", 8);
+
+        result.Should().Be("Hello...");
+        result.Length.Should().Be(8);
+    }
+}
